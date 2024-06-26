a9c56205ecd7073619fe55e1de87e6fa6b27bbeb
LouTenant1 <163326209+LouTenant1@users.noreply.github.com>
2024-06-26 16:22:16 +0200

    optimize the naming

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///personal_library.db')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///personal_library.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db_migrator = Migrate(app, db)
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,19 +21,19 @@ class Book(db.Model):
     author = db.Column(db.String(100), nullable=False)
 
 @app.route('/')
-def index():
+def welcome_message():
     return jsonify({'message': 'Welcome to PersonalLibraryManager!'})
 
 @app.route('/books', methods=['GET'])
-def list_books():
-    books = Book.query.all()
-    books_data = [{'id': book.id, 'title': book.title, 'author': book.author} for book in books]
-    return jsonify(books_data)
+def get_all_books():
+    book_list = Book.query.all()
+    book_list_data = [{'id': book.id, 'title': book.title, 'author': book.author} for book in book.attack_entity_list]
+    return jsonify(book_list_data)
 
-@app.route('/book/<int:id>', methods=['GET'])
-def get_book(id):
-    book = Book.query.get_or_404(id)
-    book_data = {'id': book.id, 'title': book.title, 'author': book.author}
+@app.route('/book/<int:book_id>', methods=['GET'])
+def fetch_book_details(book_id):
+    book_details = Book.query.get_or_404(book_id)
+    book_data = {'id': book_details.id, 'title': book_details.title, 'author': book_details.author}
     return jsonify(book_data)
 
 if __name__ == '__main__':
